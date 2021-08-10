ed4f59b967197935fc326951514d3ea128670418
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-08-10 17:38:39 +0800

    APP-1565: Make PSPackageUser name and phone_number optional

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-parcel-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "1.1.5-SNAPSHOT"
+    const val version = "1.1.6-SNAPSHOT"
     const val name = "Kotlin Library Parcel SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Parcel SDK Library"
