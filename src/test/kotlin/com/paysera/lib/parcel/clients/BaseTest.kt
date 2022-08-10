5e4a807b36b2e195532eb747cce9b9ca4d863ce4
Gilbert Parreno <gilbertparreno95@gmail.com>
2022-08-10 14:18:53 +0800

    APP-3530: Update certificate transparency across all SDKs (use JVM)

--- src/test/kotlin/com/paysera/lib/parcel/clients/BaseTest.kt
+++ src/test/kotlin/com/paysera/lib/parcel/clients/BaseTest.kt
@@ -49,7 +49,8 @@ open class BaseTest {
             credentials = apiCredentials,
             timeout = timeout,
             httpLoggingInterceptorLevel = loggingLevel,
-            errorLogger = errorLoggerInterface
+            errorLogger = errorLoggerInterface,
+            certificateInterceptor = null
         ).createClient(tokenRefresher)
     }
 
