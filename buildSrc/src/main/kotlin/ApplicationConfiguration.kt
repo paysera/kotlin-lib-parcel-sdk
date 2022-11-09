6d8f630265ea630c8f8f7a22dbd628510ecefef8
GIlbert Parreño <gilbertparreno95@gmail.com>
2022-11-09 11:49:05 +0800

    APP-3649: Implement InRento API (#22)
    * APP-3649: Implement InRento API
    
    * refactor api credentials
    
    * remove snapshot

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-parcel-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "1.1.15"
+    const val version = "1.1.16"
     const val name = "Kotlin Library Parcel SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Parcel SDK Library"
